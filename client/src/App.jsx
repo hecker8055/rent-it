ec34481a8571a27ee40ce83876c3820dd649a47c
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2023-11-13 17:01:23 +0530

    header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from './pages/Signin';
 import SignUp from './pages/signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 
 
@@ -11,6 +12,7 @@ import Profile from './pages/Profile';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/Signin' element={<SignIn/>}/>
