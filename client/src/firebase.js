9c2dbdcbb6bdb82a1d1589117d34c5ef9345d9ea
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2023-11-18 16:09:05 +0530

    google auth added

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDC24-ufX2AXz4nkSCaWJM2FE6QuN9ZFYw",
+  authDomain: "rent-it-31bf3.firebaseapp.com",
+  projectId: "rent-it-31bf3",
+  storageBucket: "rent-it-31bf3.appspot.com",
+  messagingSenderId: "388366432465",
+  appId: "1:388366432465:web:9a38efa87d83f30e876df0"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { Signin, Signup } from '../controller/authController.js';
+import { google,Signin, Signup } from '../controller/authController.js';
 
 const router = express.Router();
 
 router.post("/Signup" ,Signup);
 router.post("/Signin" ,Signin);
+router.post('/google', google)
 
 export default router;
